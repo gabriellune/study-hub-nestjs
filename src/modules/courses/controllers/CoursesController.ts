47224ca1df9b3c16dc87fbc16475e82976affbe5
gabriellune <gabriellune@outlook.com>
2023-07-12 09:30:21 -0300

    docs(swagger): implement swagger first steps

--- src/modules/courses/controllers/CoursesController.ts
+++ src/modules/courses/controllers/CoursesController.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('courses')
+@ApiTags('Courses')
 export class CoursesController {
   @Get()
   findAll(): string {
--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
+@ApiTags('Users')
 export class UsersController {
   @Get()
   findAll(): string {
